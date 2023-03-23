fa5bf22b51dff136a650d4e22af1228fedcb8c6e
Chambers <bobhus394@gmail.com>
2023-03-23 13:07:23 +0800

    新增允許網域名稱

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
 	'http://localhost:8080',
-  'https://kfc.justlikeboss.com'
+  'https://kfc.justlikeboss.com',
+  'https://nosegates.com'
 ]
 
 module.exports = allowedOrigins
